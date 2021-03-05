9c19817ed603054737df08672f171fd1e207e549
Josh Nussbaum <joshnuss@gmail.com>
2021-03-05 14:02:52 -0500

    Adds title prop to Screen component

--- src/Screen.svelte
+++ src/Screen.svelte
@@ -1,12 +1,22 @@
+<script>
+  export let title = null
+</script>
+
 <section class="terminal">
+  <div class="buttons">
+    <svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
+      <g fill="none" fill-rule="evenodd" transform="translate(1 1)">
+        <circle cx="5" cy="5" r="5" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"/>
+        <circle cx="25" cy="5" r="5" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"/>
+        <circle cx="45" cy="5" r="5" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"/>
+      </g>
+    </svg>
+  </div>
+
 	<div class="bar">
-		<svg xmlns="http://www.w3.org/2000/svg" width="54" height="14" viewBox="0 0 54 14">
-			<g fill="none" fill-rule="evenodd" transform="translate(1 1)">
-				<circle cx="6" cy="6" r="6" fill="#FF5F56" stroke="#E0443E" stroke-width=".5"/>
-				<circle cx="26" cy="6" r="6" fill="#FFBD2E" stroke="#DEA123" stroke-width=".5"/>
-				<circle cx="46" cy="6" r="6" fill="#27C93F" stroke="#1AAB29" stroke-width=".5"/>
-			</g>
-		</svg>
+
+    <div class="title">{#if title}{title}{/if}</div>
+
 	</div>
 
   <slot/>
